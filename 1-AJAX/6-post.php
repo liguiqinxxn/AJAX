13bb0b614ebc01bd4596f32977265147b85d33a8
lgq186 <1043736808@qq.com>
2018-09-25 09:54:34 +0800

    ajax

---
+++ 1-AJAX/6-post.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liguiqin
+ * Date: 2018/7/31
+ * Time: 16:13
+ */
+$username = $_POST['username'];
+$age = $_POST['age'];
+
+echo '你的名字：'.$username.',你的年龄'.$age;
